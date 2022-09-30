81617ea80289a3a9bbae2c542ea91161697a59dc
Hendro Gunawan <112385556+Hendro10@users.noreply.github.com>
2022-09-30 10:23:11 +0700

    Add files via upload

---
+++ CollectionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CollectionController extends Controller
+{
+    public function collectionSatu()
+    {
+        $collection = collect([1, 9, 3, 4, 5, 3, 5, 7]);
+        dump($collection);
+
+    }
+
+    public function collectionDua()
+    {
+        //... isi method collectionDua
+    }
+
+    public function collectionTiga()
+    {
+        //... isi method collectionTiga
+    }
+
+    public function collectionEmpat()
+    {
+        //... isi method collectionEmpat
+    }
+
+    public function collectionLima()
+    {
+        //... isi method collectionLima
+    }
+    public function collectionEnam()
+    {
+        //... isi method collectionEnam
+    }
+}
